5f89f0fbc309ee350ac1ab36e5787a45c204278f
Muddz <aux-cc@hotmail.com>
2017-01-19 23:07:27 +0100

    Fixed possible memory leaks, refactoring of methods and comments

--- sample/src/main/java/com/muddzdev/viewtobitmap/MainActivity.java
+++ sample/src/main/java/com/muddzdev/viewtobitmap/MainActivity.java
@@ -2,19 +2,18 @@ package com.muddzdev.viewtobitmap;
 
 import android.Manifest;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.muddzdev.viewtobitmaplibrary.ViewToBitmap;
 import com.muddzdev.viewtobitmaplibrary.OnBitmapSaveListener;
+import com.muddzdev.viewtobitmaplibrary.ViewToBitmap;
 
 public class MainActivity extends AppCompatActivity implements OnBitmapSaveListener {
 
-    private RelativeLayout container;
+    private RelativeLayout quotePictor;
     private Toolbar toolbar;
 
     @Override
@@ -25,28 +24,29 @@ public class MainActivity extends AppCompatActivity implements OnBitmapSaveListe
         PermissionRequester permissionRequester = new PermissionRequester(this, this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         permissionRequester.request();
 
-        container = (RelativeLayout) findViewById(R.id.container);
+        quotePictor = (RelativeLayout) findViewById(R.id.container);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 
 
-    public void bitmapSave(View v) {
+    public void saveToGallery(View v) {
 
-        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        vibrator.vibrate(60);
+        ViewToBitmap image = new ViewToBitmap(quotePictor);
+        image.setOnBitmapSaveListener(this);
+        image.saveToGallery();
 
-        ViewToBitmap toBitmap = new ViewToBitmap(this, container, null, null);
-        toBitmap.setOnBitmapSaveListener(this);
-        toBitmap.saveToBitmap();
+
     }
 
 
     @Override
     public void onBitmapSaved(final boolean isSaved, final String path) {
 
-        Toast.makeText(MainActivity.this, "Bitmap Saved at; " + path, Toast.LENGTH_SHORT).show();
-
+        if (isSaved) {
+            Toast.makeText(this, "Bitmap Saved at; " + path, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- viewtobitmap/src/main/java/com/muddzdev/viewtobitmaplibrary/ViewToBitmap.java
+++ viewtobitmap/src/main/java/com/muddzdev/viewtobitmaplibrary/ViewToBitmap.java
@@ -1,6 +1,8 @@
 package com.muddzdev.viewtobitmaplibrary;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
@@ -10,6 +12,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 
@@ -35,9 +38,9 @@ import java.io.OutputStream;
 
 /**
  * <p></p>
- * The most easy way to save any View as Bitmap to the phones gallery with options as; file format, jpg quality, naming of the files and folder.
+ * The most easy way to save any View as an image to the phones gallery with options as; file format, jpg quality, naming of the files and folder.
  * The saving operation happens in a AsyncTask thread to prevent blocking the UI Thread.
- * <p>The class contains a listener that returns a boolean value confirming weather the file has been saved or not and a path to file</p>
+ * <p>The class contains an optional listener that returns a boolean value confirming weather the file has been saved or not and a path to file</p>
  */
 
 public class ViewToBitmap {
@@ -48,25 +51,26 @@ public class ViewToBitmap {
     private static final String TAG = "ViewToBitmap";
     private static final int JPG_MAX_QUALITY = 100;
 
-    private Context context;
     private View view;
     private int jpgQuality;
-    private boolean saveAsPNG, saveAsNomedia;
     private String fileName;
     private String folderName;
+    private boolean saveAsPNG, saveAsNomedia;
     private OnBitmapSaveListener onBitmapSaveListener;
 
 
-    public ViewToBitmap(@NonNull Context context) {
-        this.context = context;
+    /**
+     * @param view The view to save as image
+     */
+    public ViewToBitmap(@NonNull View view) {
+        this.view = view;
     }
 
     /**
      * @param view       The view that will be saved as Bitmap. Can also be a ViewGroup.
      * @param folderName Name of the folder/directory that will be created to store your saved Bitmaps. If null Bitmaps will be saved to DIRECTORY_PICTURES.
      */
-    public ViewToBitmap(@NonNull Context context, @NonNull View view, @Nullable String folderName) {
-        this.context = context;
+    public ViewToBitmap(@Nullable View view, @Nullable String folderName) {
         this.view = view;
         this.folderName = folderName;
     }
@@ -76,8 +80,7 @@ public class ViewToBitmap {
      * @param folderName Name of the folder/directory that will be created to store your saved Bitmaps. If null Bitmaps will be saved to DIRECTORY_PICTURES.
      * @param fileName   If null the filename will be a timestamp at saving time.
      */
-    public ViewToBitmap(@NonNull Context context, @NonNull View view, @Nullable String folderName, @Nullable String fileName) {
-        this.context = context;
+    public ViewToBitmap(@Nullable View view, @Nullable String folderName, @Nullable String fileName) {
         this.view = view;
         this.folderName = folderName;
         this.fileName = fileName;
@@ -85,16 +88,14 @@ public class ViewToBitmap {
 
 
     /**
-     * @param saveAsNomedia Saving the bitmap as .nomedia fileformat makes it invisible in the gallery.
+     * saveAsNomedia Saving the bitmap as .nomedia fileformat makes it invisible in the gallery.
      */
-    public ViewToBitmap setSaveAsNomedia(boolean saveAsNomedia) {
-        this.saveAsNomedia = saveAsNomedia;
-        return this;
+    public void saveAsNomedia() {
+        saveAsNomedia = true;
     }
 
-    public ViewToBitmap setSaveAsPNG(boolean saveAsPNG) {
-        this.saveAsPNG = saveAsPNG;
-        return this;
+    public void saveAsPNG() {
+        saveAsPNG = true;
     }
 
     /**
@@ -104,10 +105,11 @@ public class ViewToBitmap {
      *
      * @param jpgQuality
      */
-    public ViewToBitmap setJpgQuality(int jpgQuality) {
+    public void setJpgQuality(int jpgQuality) {
         this.jpgQuality = jpgQuality;
-        return this;
+
     }
+
 
     /**
      * Set the name of saved file.
@@ -115,9 +117,8 @@ public class ViewToBitmap {
      *
      * @param fileName
      */
-    public ViewToBitmap setFileName(String fileName) {
+    public void setFileName(String fileName) {
         this.fileName = fileName;
-        return this;
     }
 
 
@@ -126,9 +127,9 @@ public class ViewToBitmap {
      *
      * @param folderName
      */
-    public ViewToBitmap setFolderName(String folderName) {
+    public void setFolderName(String folderName) {
         this.folderName = folderName;
-        return this;
+
     }
 
 
@@ -137,7 +138,7 @@ public class ViewToBitmap {
         String result;
 
         if (folderName == null || folderName.isEmpty()) {
-            result = "";
+            result = Environment.DIRECTORY_PICTURES;
         } else {
             result = folderName;
         }
@@ -145,24 +146,23 @@ public class ViewToBitmap {
         return result;
     }
 
-    public ViewToBitmap setView(@NonNull View view) {
+    public void setView(View view) {
         this.view = view;
-        return this;
     }
 
+    private Context getAppContext() {
 
-    public ViewToBitmap setOnBitmapSaveListener(OnBitmapSaveListener onBitmapSaveListener) {
+        Context context = null;
+
+        if (view != null) {
+            context = view.getContext().getApplicationContext();
+        }
+
+        return context;
+    }
+
+    public void setOnBitmapSaveListener(OnBitmapSaveListener onBitmapSaveListener) {
         this.onBitmapSaveListener = onBitmapSaveListener;
-        return this;
-    }
-
-    /**
-     * Saves the view as a bitmap in AsyncTask to the phones gallery/Environment.DIRECTORY_PICTURES
-     */
-    public ViewToBitmap saveToBitmap() {
-        AsyncSaveBitmap asyncSaveBitmap = new AsyncSaveBitmap();
-        asyncSaveBitmap.execute();
-        return this;
     }
 
     private void onBitmapSavedListener(final boolean isSaved, final String path) {
@@ -222,13 +222,59 @@ public class ViewToBitmap {
 
     }
 
+    private boolean isExternalStorageReady() {
+        boolean result;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    private boolean isPermissionGranted() {
+        String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        boolean result;
+
+        if (ContextCompat.checkSelfPermission(getAppContext(), permission) == PackageManager.PERMISSION_GRANTED) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    /**
+     * Saves the view as an Image in AsyncTask to the phones gallery
+     */
+
+    public void saveToGallery() {
+
+        if (isExternalStorageReady() && isPermissionGranted()) {
+            AsyncSaveBitmap asyncSaveBitmap = new AsyncSaveBitmap(getAppContext());
+            asyncSaveBitmap.execute();
+        } else {
+            Log.d(TAG, "Make sure external storage is available and permission WRITE_EXTERNAL_STORAGE is granted");
+        }
+
+    }
+
 
     private class AsyncSaveBitmap extends AsyncTask<Void, Void, Void> implements MediaScannerConnection.OnScanCompletedListener {
+
+        private final Context context;
+
+        public AsyncSaveBitmap(Context context) {
+            this.context = context;
+        }
 
         @Override
         protected Void doInBackground(Void... params) {
 
-            final File myDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), getFolderName());
+            final File myDir = new File(Environment.getExternalStorageDirectory(), getFolderName());
             myDir.mkdirs();
             final File file = new File(myDir, getFileName() + getFileExtension());
             OutputStream out = null;
@@ -261,6 +307,7 @@ public class ViewToBitmap {
                         e.printStackTrace();
                     }
                 }
+
                 MediaScannerConnection.scanFile(context, new String[]{file.toString()}, null, this);
             }
 
